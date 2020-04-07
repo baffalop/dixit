3bc56fe615f54327e9d207b79109b2ed61c4ef8b
Nikita Gaidakov <ngaidakov@podfather.com>
2020-04-07 22:57:52 +0100

    From HelloWorld to Dixit, with welcome screen

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img class="logo" alt="Dixit logo" src="./assets/logo-dixit.png">
+    <WelcomeScreen />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from './components/HelloWorld.vue'
+import WelcomeScreen from './components/WelcomeScreen.vue'
 
 @Component({
   components: {
-    HelloWorld
-  }
+    WelcomeScreen,
+  },
 })
+
 export default class App extends Vue {}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .logo {
+    width: 80%;
+    max-width: 600px;
+  }
 </style>
---
+++ src/components/WelcomeScreen.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="welcome-screen">
+    <form method="post" @submit="onSubmit">
+      <label for="name">Your name:</label>
+      <input id="name" type="text" required v-model="name" />
+
+      <input type="submit" value="Begin">
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private name!: string
+
+  private onSubmit () {
+    console.log(`Got the name ${this.name}`)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
