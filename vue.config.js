d7dd02fd942032964d1685babc7a089ccd0ae7d5
Nikita Gaidakov <ngaidakov@podfather.com>
2020-04-23 00:43:05 +0100

    Configure Vue devServer to proxy websocket

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   devServer: {
-    proxy: 'http://localhost:3000',
+    proxy: {
+      '^/play': {
+        target: 'ws://localhost:3000',
+        ws: true,
+      },
+      '/': {
+        target: 'http://localhost:3000',
+        ws: false,
+      },
+    },
   },
 }
